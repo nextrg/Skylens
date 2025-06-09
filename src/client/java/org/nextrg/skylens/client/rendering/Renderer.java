916c0f26888c9982887386595c822b25444a6068
Mona <59416038+meowora@users.noreply.github.com>
2025-06-09 17:39:30 +0200

    fix: rendering on 1.21.5

--- src/client/java/org/nextrg/skylens/client/rendering/Renderer.java
+++ src/client/java/org/nextrg/skylens/client/rendering/Renderer.java
@@ -4,60 +4,87 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 import earth.terrarium.olympus.client.pipelines.RoundedRectangle;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.*;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.item.ItemStack;
 import org.joml.Matrix4f;
-import static com.mojang.blaze3d.systems.RenderSystem.*;
+
+import static com.mojang.blaze3d.systems.RenderSystem.setShaderColor;
 
 public class Renderer {
     public static void beginRendering() {
         setShaderColor(1, 1, 1, 1);
     }
-    public static void drawBuffer(BufferBuilder buf) {
-        BufferRenderer.drawWithGlobalProgram(buf.end());
-    }
-    public static void drawCircle(Matrix4f mat, int cx, int cy, float radius, int start, int end, int color, int z) {
-        BufferBuilder buf = Tessellator.getInstance().begin(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
-        buf.vertex(mat,(float)cx,(float)cy,z).color(color);
-        for (int i = start - 90; i <= end - 90; i++) {
-            double angle = Math.toRadians(i);
-            float x = (float) -(Math.cos(angle) * radius) + cx;
-            float y = (float) (Math.sin(angle) * radius) + cy;
-            buf.vertex(mat,x,y,z + 1).color(color);
-        }
-        beginRendering();
-        drawBuffer(buf);
-    }
+
     public static void legacyRoundRectangle(Matrix4f mat, float x, float y, float w, float h, float r, int color) {
         r = Math.clamp(r, 1, Math.min(w, h) / 2);
-        BufferBuilder buf = Tessellator.getInstance().begin(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
+        BufferBuilder buf = Tessellator.getInstance()
+                .begin(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
         buf.vertex(mat, x + w / 2F, y + h / 2F, 0).color(color);
         float[][] corners = {
-                { x + w - r, y + r },
-                { x + w - r, y + h - r},
-                { x + r, y + h - r },
-                { x + r, y + r }
+                {x + w - r, y + r},
+                {x + w - r, y + h - r},
+                {x + r, y + h - r},
+                {x + r, y + r}
         };
         for (int corner = 0; corner < 4; corner++) {
             int cornerStart = (corner - 1) * 90;
             int cornerEnd = cornerStart + 90;
             for (int i = cornerStart; i <= cornerEnd; i += 10) {
-                float angle = (float)Math.toRadians(i);
-                float rx = corners[corner][0] + (float)(Math.cos(angle) * r);
-                float ry = corners[corner][1] + (float)(Math.sin(angle) * r);
+                float angle = (float) Math.toRadians(i);
+                float rx = corners[corner][0] + (float) (Math.cos(angle) * r);
+                float ry = corners[corner][1] + (float) (Math.sin(angle) * r);
                 buf.vertex(mat, rx, ry, 0).color(color);
             }
         }
         buf.vertex(mat, corners[0][0], y, 0).color(color);
         beginRendering();
-        drawBuffer(buf);
     }
-    public static void roundGradient(DrawContext graphics, int x, int y, int width, int height, float borderRadius, int startColor, int endColor, int gradientDirection, float animTime, int borderWidth, int borderColor) {
-        RoundGradShader.fill(graphics, x, y, width, height, borderRadius, startColor, endColor, gradientDirection, animTime, borderWidth, borderColor);
+
+    public static void roundGradient(
+            DrawContext graphics,
+            int x,
+            int y,
+            int width,
+            int height,
+            float borderRadius,
+            int startColor,
+            int endColor,
+            int gradientDirection,
+            float animTime,
+            int borderWidth,
+            int borderColor
+    ) {
+        RoundGradShader.fill(
+                graphics,
+                x,
+                y,
+                width,
+                height,
+                borderRadius,
+                startColor,
+                endColor,
+                gradientDirection,
+                animTime,
+                borderWidth,
+                borderColor);
     }
-    public static void roundRectangle(DrawContext graphics, int x, int y, int width, int height, float borderRadius, int backgroundColor, int borderWidth, int borderColor) {
+
+    public static void roundRectangle(
+            DrawContext graphics,
+            int x,
+            int y,
+            int width,
+            int height,
+            float borderRadius,
+            int backgroundColor,
+            int borderWidth,
+            int borderColor
+    ) {
         RoundedRectangle.draw(graphics, x, y, width, height, backgroundColor, borderColor, borderRadius, borderWidth);
     }
+
     public static void drawItem(DrawContext context, ItemStack item, float x, float y, float scale) {
         context.getMatrices().push();
         context.getMatrices().translate(x, y, 0);
@@ -67,7 +94,17 @@ public class Renderer {
         context.drawItem(item, 0, 0);
         context.getMatrices().pop();
     }
-    public static void drawText(DrawContext context, String text, float x, float y, int color, float scale, boolean centered, boolean shadow) {
+
+    public static void drawText(
+            DrawContext context,
+            String text,
+            float x,
+            float y,
+            int color,
+            float scale,
+            boolean centered,
+            boolean shadow
+    ) {
         context.getMatrices().push();
         context.getMatrices().translate(x, y, 0);
         context.getMatrices().scale(scale, scale, scale);
@@ -78,12 +115,15 @@ public class Renderer {
         }
         context.getMatrices().pop();
     }
+
     public static float easeInOutQuadratic(float t) {
-        if(t <= 0.5f)
+        if (t <= 0.5f) {
             return 2.0f * t * t;
+        }
         t -= 0.5f;
         return 2.0f * t * (1.0f - t) + 0.5f;
     }
+
     public static float easeInOutCubic(float t) {
         if (t < 0.5f) {
             return 4.0f * t * t * t;
@@ -92,10 +132,13 @@ public class Renderer {
             return 1.0f - (f * f * f) / 2.0f;
         }
     }
+
     public static int getScreenWidth(DrawContext context) {
         return context.getScaledWindowWidth();
     }
+
     public static int getScreenHeight(DrawContext context) {
         return context.getScaledWindowHeight();
     }
+
 }
--- src/client/java/org/nextrg/skylens/client/rendering/RoundGradShader.java
+++ src/client/java/org/nextrg/skylens/client/rendering/RoundGradShader.java
@@ -1,123 +1,83 @@
 package org.nextrg.skylens.client.rendering;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import earth.terrarium.olympus.client.shader.Shader;
+import com.mojang.blaze3d.pipeline.BlendFunction;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.platform.DepthTestFunction;
+import com.mojang.blaze3d.platform.LogicOp;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import earth.terrarium.olympus.client.pipelines.PipelineRenderer;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.RenderPipelines;
+import net.minecraft.client.gl.UniformType;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.*;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.Window;
 import org.joml.Matrix4f;
-import org.joml.Vector2f;
-import org.joml.Vector4f;
-import java.util.function.Supplier;
+import org.nextrg.skylens.client.SkylensClient;
 
 public class RoundGradShader {
-    private static Supplier<String> VERTEX = () -> "#version 150\n\nin vec3 Position;\n\nuniform mat4 modelViewMat;\nuniform mat4 projMat;\n\nvoid main() {\n    gl_Position = projMat * modelViewMat * vec4(Position, 1.0);\n}\n";
-    private static Supplier<String> FRAGMENT = () -> """
-    #version 150
-    uniform mat4 modelViewMat;
-    uniform mat4 projMat;
-    uniform vec4 startColor;
-    uniform vec4 endColor;
-    uniform vec4 borderColor;
-    uniform vec4 borderRadius;
-    uniform float borderWidth;
-    uniform vec2 size;
-    uniform vec2 center;
-    uniform float scaleFactor;
-    uniform int gradientDirection;
-    uniform float time;
-    out vec4 fragColor;
-    float sdRoundedBox(vec2 p, vec2 b, vec4 r){
-        r.xy = (p.x > 0.0) ? r.xy : r.zw;
-        r.x  = (p.y > 0.0) ? r.x  : r.y;
-        vec2 q = abs(p)-b+r.x;
-        return min(max(q.x,q.y),0.0) + length(max(q,0.0)) - r.x;
+    private static final RenderPipeline PROGRESS_CHART = RenderPipelines.register(
+            RenderPipeline.builder()
+                    .withLocation(SkylensClient.id("round_grad"))
+                    .withVertexShader(SkylensClient.id("core/round_grad"))
+                    .withFragmentShader(SkylensClient.id("core/round_grad"))
+                    .withCull(false)
+                    .withDepthTestFunction(DepthTestFunction.NO_DEPTH_TEST)
+                    .withColorLogic(LogicOp.NONE)
+                    .withBlend(BlendFunction.TRANSLUCENT)
+                    .withVertexFormat(VertexFormats.POSITION_TEXTURE_COLOR, VertexFormat.DrawMode.QUADS)
+                    .withSampler("Sampler0")
+                    .withUniform("Size", UniformType.INT)
+                    .withUniform("Vertical", UniformType.INT)
+                    .build()
+    );
+
+    public RoundGradShader() {
     }
-    void main() {
-        vec2 halfSize = size / 2.0;
-        vec2 pos = gl_FragCoord.xy - center;
-        float distance = sdRoundedBox(pos, halfSize, borderRadius * scaleFactor);
-        float gradientFactor;
-        
-        float gradientOffset = fract(time);
-        if (gradientDirection == 0) {
-            gradientFactor = (pos.y + halfSize.y) / size.y + gradientOffset;
-        } else if (gradientDirection == 1) {
-            gradientFactor = (pos.x + halfSize.x) / size.x + gradientOffset;
-        } else {
-            gradientFactor = ((pos.x + halfSize.x) + (pos.y + halfSize.y)) / (size.x + size.y) + gradientOffset;
-        }
-        gradientFactor = fract(gradientFactor);
-        
-        vec4 color;
-        vec4 colorA = startColor;
-        vec4 colorB = endColor;
-        vec4 colorC = startColor;
-        
-        if (gradientFactor < 0.5) {
-            color = mix(colorA, colorB, gradientFactor * 2.0);
-        } else {
-            color = mix(colorB, colorC, (gradientFactor - 0.5) * 2.0);
-        }
-        float smoothed = min(1.0 - distance, color.a);
-        float border = min(1.0 - smoothstep(borderWidth, borderWidth, abs(distance)), borderColor.a);
-        if (border > 0.0) {
-            fragColor = borderColor * vec4(1.0, 1.0, 1.0, border);
-        } else {
-            fragColor = color * vec4(1.0, 1.0, 1.0, smoothed);
-        }
-    }
-    """;
-    public static Shader<RoundGradUniform> SHADER;
-    
-    public RoundGradShader() {}
-    
-    public static void fill(DrawContext graphics, int x, int y, int width, int height, float radius,
-                            int startColor, int endColor, int gradientDirection, float animationTime, int borderWidth, int borderColor) {
+
+    public static void fill(
+            DrawContext graphics, int x, int y, int width, int height, float radius,
+            int startColor, int endColor, int gradientDirection, float animationTime, int borderWidth, int borderColor
+    ) {
         Window window = MinecraftClient.getInstance().getWindow();
-        float scale = (float)window.getScaleFactor();
-        float scaledX = (float)x * scale;
-        float scaledY = (float)y * scale;
-        float scaledWidth = (float)width * scale;
-        float scaledHeight = (float)height * scale;
-        float yOffset = (float)window.getFramebufferHeight() - scaledHeight - scaledY * 2.0F;
-        RoundGradUniform uniforms = (RoundGradUniform)SHADER.uniforms();
-        uniforms.modelViewMat.set(new Matrix4f(RenderSystem.getModelViewMatrix()));
-        uniforms.projMat.set(new Matrix4f(RenderSystem.getProjectionMatrix()));
-        uniforms.startColor.set(colorToVec4f(startColor));
-        uniforms.endColor.set(colorToVec4f(endColor));
-        uniforms.gradientDirection.set(gradientDirection);
-        uniforms.borderColor.set(new Vector4f((float)(borderColor >> 16 & 255) / 255.0F, (float)(borderColor >> 8 & 255) / 255.0F, (float)(borderColor & 255) / 255.0F, (float)(borderColor >> 24 & 255) / 255.0F));
-        uniforms.borderRadius.set(new Vector4f(radius, radius, radius, radius));
-        uniforms.borderWidth.set((float)borderWidth);
-        uniforms.size.set(new Vector2f(scaledWidth - (float)borderWidth * 2.0F * scale, scaledHeight - (float)borderWidth * 2.0F * scale));
-        uniforms.center.set(new Vector2f(scaledX + scaledWidth / 2.0F, scaledY + scaledHeight / 2.0F + yOffset));
-        uniforms.scaleFactor.set(scale);
-        uniforms.time.set(animationTime);
-        SHADER.use(() -> {
-            RenderSystem.enableBlend();
-            Matrix4f matrix = graphics.getMatrices().peek().getPositionMatrix();
-            BufferBuilder buffer = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
-            buffer.vertex(matrix, (float)x, (float)y, 0.0F);
-            buffer.vertex(matrix, (float)x, (float)(y + height), 0.0F);
-            buffer.vertex(matrix, (float)(x + width), (float)(y + height), 0.0F);
-            buffer.vertex(matrix, (float)(x + width), (float)y, 0.0F);
-            BufferRenderer.draw(buffer.end());
-            RenderSystem.disableBlend();
-        });
+        float scale = (float) window.getScaleFactor();
+        float scaledX = (float) x * scale;
+        float scaledY = (float) y * scale;
+        float scaledWidth = (float) width * scale;
+        float scaledHeight = (float) height * scale;
+        float yOffset = (float) window.getFramebufferHeight() - scaledHeight - scaledY * 2.0F;
+
+        Matrix4f matrix = graphics.getMatrices().peek().getPositionMatrix();
+        BufferBuilder buffer = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
+
+        buffer.vertex(matrix, x, y, 0.0F);
+        buffer.vertex(matrix, x, (y + height), 0.0F);
+        buffer.vertex(matrix, (x + width), (y + height), 0.0F);
+        buffer.vertex(matrix, (x + width), y, 0.0F);
+        PipelineRenderer.draw(
+                PROGRESS_CHART, buffer.end(), pass -> {
+                    pass.setUniform("startColor", colorToVec4f(startColor));
+                    pass.setUniform("endColor", colorToVec4f(endColor));
+                    pass.setUniform("center", scaledX, scaledY + yOffset);
+                    pass.setUniform("gradientDirection", gradientDirection);
+                    pass.setUniform("borderColor", (float) (borderColor >> 16 & 255) / 255.0F, (float) (borderColor >> 8 & 255) / 255.0F, (float) (borderColor & 255) / 255.0F, (float) (borderColor >> 24 & 255) / 255.0F);
+                    pass.setUniform("borderRadius", radius, radius, radius, radius);
+                    pass.setUniform("borderWidth", borderWidth);
+                    pass.setUniform("size", scaledWidth - (float) borderWidth * 2.0F * scale, scaledHeight - (float) borderWidth * 2.0F * scale);
+                    pass.setUniform("center", scaledX + scaledWidth / 2.0F, scaledY + scaledHeight / 2.0F + yOffset);
+                    pass.setUniform("borderColor", colorToVec4f(borderColor));
+                    pass.setUniform("time", animationTime);
+                });
     }
-    
-    private static Vector4f colorToVec4f(int color) {
-        return new Vector4f(
+
+    private static float[] colorToVec4f(int color) {
+        return new float[]{
                 (color >> 16 & 0xFF) / 255f,
-                (color >> 8  & 0xFF) / 255f,
-                (color       & 0xFF) / 255f,
+                (color >> 8 & 0xFF) / 255f,
+                (color & 0xFF) / 255f,
                 (color >> 24 & 0xFF) / 255f
-        );
-    }
-    
-    static {
-        SHADER = Shader.make(VERTEX, FRAGMENT, RoundGradUniform::new);
+        };
     }
 }
